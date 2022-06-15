87d2454962eb1a250a15b7bd356f2ca653214b40
Jessica Kitchen <jess@Jessicas-Mini.lan>
2022-06-14 22:42:03 -0400

    completed social media menu bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
-import {FaBars, FaTimes} from 'react-icons/fa';
+import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa';
+import {HiOutlineMail} from 'react-icons/hi';
+import {BsFillPersonLinesFill} from 'react-icons/bs';
 import Logo from '../assets/real logo.png';
 
 const Navbar = () => {
@@ -23,7 +25,7 @@ const Navbar = () => {
 
         {/* hamburger */}
         <div onClick={handleClick} className='md:hidden z-10'>
-            <FaBars />
+            {!nav ? <FaBars /> : <FaTimes />}
         </div>
             {/* mobile menu */}
             <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#7562a1] flex flex-col justify-center items-center'}>
@@ -35,7 +37,31 @@ const Navbar = () => {
             </ul>
 
             {/* social icons */}
-            <div></div>
+            <div className='flex fixed flex-col top-[35%] left-0'>
+                <ul>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-105px] hover:ml-[0px] duration-900 bg-[#0072b1]'>
+                        <a className='flex justify-between items-center w-full text- white' href="https://www.linkedin.com/in/jesswillco/">
+                            LinkedIn <FaLinkedin size={30} />
+                        </a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-105px] hover:ml-[0px] duration-900 bg-[#443c41]'>
+                        <a className='flex justify-between items-center w-full text- white' href="https://www.linkedin.com/in/jesswillco/">
+                            Github <FaGithub size={30} />
+                        </a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-105px] hover:ml-[0px] duration-900 bg-[#a2d3de]'>
+                        <a className='flex justify-between items-center w-full text- white' href="https://www.linkedin.com/in/jesswillco/">
+                            Email <HiOutlineMail size={30} />
+                        </a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-105px] hover:ml-[0px] duration-900 bg-[#7562a1]'>
+                        <a className='flex justify-between items-center w-full text- white' href="https://www.linkedin.com/in/jesswillco/">
+                            Resume <BsFillPersonLinesFill size={30} />
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
     </div>
   )
 }
