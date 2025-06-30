364dfee23174575363c220c42161068ab441abee
Jessica Williams <jesswillcode@gmail.com>
2025-06-30 18:16:33 -0400

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-// import Coder from '../assets/Coderheroes.png';
-// import Disney from '../assets/DisneySnackPlanner.png';
-// import Waifu from '../assets/WaifuforYou.png';
-// import Recipe from '../assets/SecretFamilyRecipe.png';
 
 const Work = () => {
   return (
@@ -12,74 +8,6 @@ const Work = () => {
                 <p className='text-4xl text-[#7562a1] font-[Amsterdam4] font-bold inline border-b-4 border-[#2a145b] pb-8'>Work</p>
                   <p className='text-4xl'>Excuse the Mess! Updated Content Coming Soon.</p
             </div>
-
-              {/* <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4 mx-20 px-20'>
-                <div style={{backgroundImage: `url(${Coder})`}} 
-                className='group container rounded-md flex justify-center items-center content-div'>
-
-                    <div className='opacity-0 group-hover:opacity-100'>
-                            <div className='pt-8 text-center'>
-                                <a target='_blank' rel='noreferrer'  href="https://a.coderheroes.dev/login">
-                                    <button className='border-2 border-white text-center rounded-lg px-4 py-3 m-2 bg-[#7562a1] text-white font-bold text-lg hover:bg-[#a2d3de] hover:border-[#2a145b] hover:border-2'>Demo</button>
-                                </a>
-                                <a target='_blank' rel='noreferrer'  href="https://github.com/JessWillCode/coder-heroes-fe">
-                                    <button className='border-2 border-white text-center rounded-lg px-4 py-3 m-2 bg-[#7562a1] text-white font-bold text-lg hover:bg-[#a2d3de] hover:border-[#2a145b] hover:border-2'>Code</button>
-                                </a>
-
-                            </div>
-                        </div>
-                    
-                </div>
-                <div style={{backgroundImage: `url(${Disney})`}} 
-                className='group container rounded-md flex justify-center items-center content-div'>
-
-                    <div className='opacity-0 group-hover:opacity-100'>
-                            <div className='pt-8 text-center'>
-                                <a target='_blank' rel='noreferrer'  href="/">
-                                    <button className='border-2 border-white text-center rounded-lg px-4 py-3 m-2 bg-[#7562a1] text-white font-bold text-lg hover:bg-[#a2d3de] hover:border-[#2a145b] hover:border-2'>Demo</button>
-                                </a>
-                                <a target='_blank' rel='noreferrer'  href="https://github.com/JessWillCode/disney-snack-planner">
-                                    <button className='border-2 border-white text-center rounded-lg px-4 py-3 m-2 bg-[#7562a1] text-white font-bold text-lg hover:bg-[#a2d3de] hover:border-[#2a145b] hover:border-2'>Code</button>
-                                </a>
-
-                            </div>
-                        </div>
-                    
-                </div>
-                <div style={{backgroundImage: `url(${Recipe})`}} 
-                className='group container rounded-md flex justify-center items-center content-div'>
-
-                    <div className='opacity-0 group-hover:opacity-100'>
-                            <div className='pt-8 text-center'>
-                                <a target='_blank' rel='noreferrer'  href="/">
-                                    <button className='border-2 border-white text-center rounded-lg px-4 py-3 m-2 bg-[#7562a1] text-white font-bold text-lg hover:bg-[#a2d3de] hover:border-[#2a145b] hover:border-2'>Demo</button>
-                                </a>
-                                <a target='_blank' rel='noreferrer'  href="https://github.com/JessWillCode/secret-fam-recipes">
-                                    <button className='border-2 border-white text-center rounded-lg px-4 py-3 m-2 bg-[#7562a1] text-white font-bold text-lg hover:bg-[#a2d3de] hover:border-[#2a145b] hover:border-2'>Code</button>
-                                </a>
-
-                            </div>
-                        </div>
-                    
-                </div>
-                <div style={{backgroundImage: `url(${Waifu})`}} 
-                className='group container rounded-md flex justify-center items-center content-div'>
-
-                    <div className='opacity-0 group-hover:opacity-100'>
-                            <div className='pt-8 text-center'>
-                                <a target='_blank' rel='noreferrer'  href="/">
-                                    <button className='border-2 border-white text-center rounded-lg px-4 py-3 m-2 bg-[#7562a1] text-white font-bold text-lg hover:bg-[#a2d3de] hover:border-[#2a145b] hover:border-2'>Demo</button>
-                                </a>
-                                <a target='_blank' rel='noreferrer'  href="https://github.com/JessWillCode/waifus-for-you">
-                                    <button className='border-2 border-white text-center rounded-lg px-4 py-3 m-2 bg-[#7562a1] text-white font-bold text-lg hover:bg-[#a2d3de] hover:border-[#2a145b] hover:border-2'>Code</button>
-                                </a>
-
-                            </div>
-                        </div>
-                    
-                </div>
-            </div> */}
-
         </div>
     </div>
   )
