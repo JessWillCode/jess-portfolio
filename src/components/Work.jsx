d53d7c967b9a4eb6bce26299a095db14d22f1c45
Jessica Williams <jesswillcode@gmail.com>
2025-06-30 18:30:47 -0400

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,7 +5,7 @@ const Work = () => {
     <div name='work' className='w-full md:h-screen'>
           <div className='flex flex-col justify-center items-center w-full h-full'>
             <p className='text-4xl text-[#7562a1] font-[Amsterdam4] font-bold inline border-b-4 border-[#2a145b] pb-8'>Work</p>
-            <p className='text-4xl'>Excuse the Mess! Updated Content Coming Soon.</p>
+            <p className='text-4xl pt-8'>Excuse the Mess! Updated Content Coming Soon.</p>
         </div>
     </div>
   )
