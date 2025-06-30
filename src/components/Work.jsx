05a47fda301242be8fee81b2e41070be2e2efe06
Jessica Williams <jesswillcode@gmail.com>
2025-06-30 18:19:42 -0400

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,11 +3,9 @@ import React from 'react';
 const Work = () => {
   return (
     <div name='work' className='w-full md:h-screen'>
-        <div>
-            <div className='flex flex-col justify-center items-center w-full h-full'>
-                <p className='text-4xl text-[#7562a1] font-[Amsterdam4] font-bold inline border-b-4 border-[#2a145b] pb-8'>Work</p>
-                  <p className='text-4xl'>Excuse the Mess! Updated Content Coming Soon.</p
-            </div>
+          <div className='flex flex-col justify-center items-center w-full h-full'>
+            <p className='text-4xl text-[#7562a1] font-[Amsterdam4] font-bold inline border-b-4 border-[#2a145b] pb-8'>Work</p>
+            <p className='text-4xl'>Excuse the Mess! Updated Content Coming Soon.</p>
         </div>
     </div>
   )
